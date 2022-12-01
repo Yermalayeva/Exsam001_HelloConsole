2db22609218197134728e0267bb79a673bd13ba6
Natalya Yermolaeva <natali.ermol73@mail.ru>
2022-12-01 17:00:22 +0300

    feat: add Exam 005 Name

---
+++ Exam005_Name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "наталья")
+{
+    Console.Write("Ура, это же Наталья");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
